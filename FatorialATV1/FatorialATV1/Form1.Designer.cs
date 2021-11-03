6639687acf2ddd1411c6f4e5c9f4232e17c81ba1
Renata Pedroso <renatanicollyy1@gmail.com>
2021-11-03 13:46:30 -0300

    Programa para calcular e apresentar o fatorial de um número escolhido

---
+++ FatorialATV1/FatorialATV1/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace FatorialATV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstFatorial = new System.Windows.Forms.ListBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstFatorial
+            // 
+            this.lstFatorial.FormattingEnabled = true;
+            this.lstFatorial.Location = new System.Drawing.Point(48, 144);
+            this.lstFatorial.Name = "lstFatorial";
+            this.lstFatorial.Size = new System.Drawing.Size(120, 251);
+            this.lstFatorial.TabIndex = 0;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(216, 77);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "button1";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(48, 79);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(120, 20);
+            this.txtNumero.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lstFatorial);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstFatorial;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtNumero;
+    }
+}
+
---
+++ FatorialATV1/FatorialATV1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FatorialATV1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int numero, contador;
+            int fatorial = 1;
+            
+            int.TryParse(txtNumero.Text, out numero);
+
+            for (contador = numero; contador >= 1; contador--)
+            {
+                fatorial *= contador;
+                lstFatorial.Items.Add(contador);
+            }
+            lstFatorial.Items.Add(numero + "!" + " = " + fatorial);
+        }
+    }
+}
