fe92137e42b88d2154181e60687f5decfa864c52
Renata Pedroso <renatanicollyy1@gmail.com>
2021-11-03 13:45:40 -0300

    Programa para treinar o uso de radiobutton e listbox

---
+++ EscolhaATV1/EscolhaATV1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace EscolhaATV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radD = new System.Windows.Forms.RadioButton();
+            this.radI = new System.Windows.Forms.RadioButton();
+            this.radE = new System.Windows.Forms.RadioButton();
+            this.cboOpcoes = new System.Windows.Forms.ComboBox();
+            this.picImg = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picImg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radD
+            // 
+            this.radD.AutoSize = true;
+            this.radD.Location = new System.Drawing.Point(77, 77);
+            this.radD.Name = "radD";
+            this.radD.Size = new System.Drawing.Size(107, 17);
+            this.radD.TabIndex = 0;
+            this.radD.TabStop = true;
+            this.radD.Text = "Desenvolvimento";
+            this.radD.UseVisualStyleBackColor = true;
+            this.radD.CheckedChanged += new System.EventHandler(this.radD_CheckedChanged);
+            // 
+            // radI
+            // 
+            this.radI.AutoSize = true;
+            this.radI.Location = new System.Drawing.Point(77, 115);
+            this.radI.Name = "radI";
+            this.radI.Size = new System.Drawing.Size(87, 17);
+            this.radI.TabIndex = 1;
+            this.radI.TabStop = true;
+            this.radI.Text = "Infraestrutura";
+            this.radI.UseVisualStyleBackColor = true;
+            this.radI.CheckedChanged += new System.EventHandler(this.radI_CheckedChanged);
+            // 
+            // radE
+            // 
+            this.radE.AutoSize = true;
+            this.radE.Location = new System.Drawing.Point(77, 154);
+            this.radE.Name = "radE";
+            this.radE.Size = new System.Drawing.Size(96, 17);
+            this.radE.TabIndex = 2;
+            this.radE.TabStop = true;
+            this.radE.Text = "Entretenimento";
+            this.radE.UseVisualStyleBackColor = true;
+            this.radE.CheckedChanged += new System.EventHandler(this.radE_CheckedChanged);
+            // 
+            // cboOpcoes
+            // 
+            this.cboOpcoes.FormattingEnabled = true;
+            this.cboOpcoes.Location = new System.Drawing.Point(77, 202);
+            this.cboOpcoes.Name = "cboOpcoes";
+            this.cboOpcoes.Size = new System.Drawing.Size(121, 21);
+            this.cboOpcoes.TabIndex = 3;
+            this.cboOpcoes.SelectedIndexChanged += new System.EventHandler(this.cboOpcoes_SelectedIndexChanged);
+            // 
+            // picImg
+            // 
+            this.picImg.Location = new System.Drawing.Point(249, 39);
+            this.picImg.Name = "picImg";
+            this.picImg.Size = new System.Drawing.Size(284, 235);
+            this.picImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picImg.TabIndex = 4;
+            this.picImg.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picImg);
+            this.Controls.Add(this.cboOpcoes);
+            this.Controls.Add(this.radE);
+            this.Controls.Add(this.radI);
+            this.Controls.Add(this.radD);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picImg)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radD;
+        private System.Windows.Forms.RadioButton radI;
+        private System.Windows.Forms.RadioButton radE;
+        private System.Windows.Forms.ComboBox cboOpcoes;
+        private System.Windows.Forms.PictureBox picImg;
+    }
+}
+
---
+++ EscolhaATV1/EscolhaATV1/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EscolhaATV1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radD_CheckedChanged(object sender, EventArgs e)
+        {
+            cboOpcoes.Items.Clear();
+            cboOpcoes.Items.Add("Python");
+            cboOpcoes.Items.Add("Java");
+            cboOpcoes.Items.Add("C#");
+            cboOpcoes.Items.Add("Java Script");
+            cboOpcoes.Items.Add("PHP");
+
+        }
+
+        private void radI_CheckedChanged(object sender, EventArgs e)
+        {
+            cboOpcoes.Items.Clear();
+            cboOpcoes.Items.Add("Azure");
+            cboOpcoes.Items.Add("AWS");
+            cboOpcoes.Items.Add("Cisco");
+            cboOpcoes.Items.Add("Dell");
+            cboOpcoes.Items.Add("Asus");
+        }
+
+        private void radE_CheckedChanged(object sender, EventArgs e)
+        {
+            cboOpcoes.Items.Clear();
+            cboOpcoes.Items.Add("Meu Pai");
+            cboOpcoes.Items.Add("Judas e o Messias Negro");
+            cboOpcoes.Items.Add("Mank");
+            cboOpcoes.Items.Add("Minari");
+            cboOpcoes.Items.Add("Nomadland");
+        }
+
+        private void cboOpcoes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Desenvolvimento
+            if (cboOpcoes.SelectedItem == "Python")
+            {
+                picImg.Load("python.png");
+            }
+            else if (cboOpcoes.SelectedItem == "Java")
+            {
+                picImg.Load("Java.png");
+            }
+            else if (cboOpcoes.SelectedItem == "C#")
+            {
+                picImg.Load("c.png");
+            }
+            else if (cboOpcoes.SelectedItem == "Java Script")
+            {
+                picImg.Load("javascript.png");
+            }
+            else if (cboOpcoes.SelectedItem == "PHP")
+            {
+                picImg.Load("php.png");
+            }
+
+            //Infraestrutura
+            else if (cboOpcoes.SelectedItem == "Azure")
+            {
+                picImg.Load("azure.png");
+            }
+            else if (cboOpcoes.SelectedItem == "AWS")
+            {
+                picImg.Load("aws.png");
+            }
+            else if (cboOpcoes.SelectedItem == "Cisco")
+            {
+                picImg.Load("cisco.png");
+            }
+            else if (cboOpcoes.SelectedItem == "Dell")
+            {
+                picImg.Load("dell.png");
+            }
+            else if (cboOpcoes.SelectedItem == "Asus")
+            {
+                picImg.Load("asus.png");
+            }
+
+            //Entretenimento
+            else if (cboOpcoes.SelectedItem == "Meu Pai")
+            {
+                picImg.Load("meupai.png");
+            }
+            else if (cboOpcoes.SelectedItem == "Judas e o Messias Negro")
+            {
+                picImg.Load("judas.png");
+            }
+            else if (cboOpcoes.SelectedItem == "Mank")
+            {
+                picImg.Load("mank.png");
+            }
+            else if (cboOpcoes.SelectedItem == "Minari")
+            {
+                picImg.Load("minari.png");
+            }
+            else if (cboOpcoes.SelectedItem == "Nomadland")
+            {
+                picImg.Load("nomadland.png");
+            }
+        }
+    }
+}
