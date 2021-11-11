ecd6327c81e58dae03e146851f84ff1c1abcc662
Renata Pedroso <renatanicollyy1@gmail.com>
2021-11-10 22:24:20 -0300

    matrizes

---
+++ vetoresMatrizes/vetoresMatrizes/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace vetoresMatrizes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lstVetor = new System.Windows.Forms.ListBox();
+            this.btnMatriz = new System.Windows.Forms.Button();
+            this.lstMatriz = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(46, 55);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "button1";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lstVetor
+            // 
+            this.lstVetor.FormattingEnabled = true;
+            this.lstVetor.Location = new System.Drawing.Point(46, 135);
+            this.lstVetor.Name = "lstVetor";
+            this.lstVetor.Size = new System.Drawing.Size(120, 199);
+            this.lstVetor.TabIndex = 1;
+            // 
+            // btnMatriz
+            // 
+            this.btnMatriz.Location = new System.Drawing.Point(240, 54);
+            this.btnMatriz.Name = "btnMatriz";
+            this.btnMatriz.Size = new System.Drawing.Size(75, 23);
+            this.btnMatriz.TabIndex = 2;
+            this.btnMatriz.Text = "matriz";
+            this.btnMatriz.UseVisualStyleBackColor = true;
+            this.btnMatriz.Click += new System.EventHandler(this.btnMatriz_Click);
+            // 
+            // lstMatriz
+            // 
+            this.lstMatriz.FormattingEnabled = true;
+            this.lstMatriz.Location = new System.Drawing.Point(240, 135);
+            this.lstMatriz.Name = "lstMatriz";
+            this.lstMatriz.Size = new System.Drawing.Size(120, 199);
+            this.lstMatriz.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstMatriz);
+            this.Controls.Add(this.btnMatriz);
+            this.Controls.Add(this.lstVetor);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ListBox lstVetor;
+        private System.Windows.Forms.Button btnMatriz;
+        private System.Windows.Forms.ListBox lstMatriz;
+    }
+}
+
---
+++ vetoresMatrizes/vetoresMatrizes/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vetoresMatrizes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int [] vetor = { 5, 2, 8, 9 };
+            int elemento = 0;
+
+            for (int i = 0; i < vetor.Length; i++ )
+            {
+                lstVetor.Items.Add("Elemento " + elemento + " = " + vetor[i]);
+                elemento++;
+            }
+        }
+
+        private void btnMatriz_Click(object sender, EventArgs e)
+        {
+            int[,] matrizB = { { 2 , 4 , 6 , 8 }, { 1 , 3 , 5 , 7 } };
+
+            for (int i = 0; i <= 1; i++)
+            {
+                for (int j = 0; j <= 3; j++)
+                {
+                    lstMatriz.Items.Add("Elemento [" + i + "," + j + " ] = " + matrizB[i, j]);
+                }
+            }
+        }
+    }
+}
